48a99e1702f33bb07fe74fe952de363555b5f6b9
camila de o macedo <cmacedo@camilas-MBP.connect>
2018-05-01 07:34:28 +0100

    [fix] - Unable to run grunt tasks with NodeJS 8

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,9 +19,11 @@ module.exports = function(grunt) {
   grunt.initConfig({
     _test_runner: '_mocha',
     _istanbul: 'istanbul cover --dir',
-    _unit_args: '--recursive -t 10000 ./test/',
+    _unit_args: '--recursive -t 10000 ./test/unit',
     unit: '<%= _test_runner %> <%= _unit_args %>',
     unit_cover: '<%= _istanbul %> cov-unit <%= _test_runner %> -- <%= _unit_args %>',
+    integrate:['<%= _test_runner %> ./test/integrate'],
+    accept:['<%= _test_runner %> ./test/accept'],
     mochaTest: {
       all: ['./test/*.js']
     }
